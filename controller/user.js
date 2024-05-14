1ec378d0e6562ea0d7403bcdb3f8695353788412
yemce <yunusemreclk0hfl@gmail.com>
2024-05-14 18:46:12 +0300

    User pages route has been created.

--- controller/user.js
+++ controller/user.js
@@ -2,3 +2,16 @@ exports.homePage = (req,res,next)=>{
     res.render("user/home-page");
 
 }
+
+exports.list_advert= (req,res,next)=>{
+    res.send("ilan listeleme sayfası");
+}
+
+exports.view_advert= (req,res,next)=>{
+    res.send("ilana gözatma sayfası");
+}
+
+exports.add_advert= (req,res,next)=>{
+    res.send("ilan ekleme sayfası");
+}
+
--- routes/user.js
+++ routes/user.js
@@ -4,4 +4,11 @@ const userController = require("../controller/user");
 
 router.get("/",userController.homePage);
 
+router.get("/list/advert",userController.list_advert);
+
+router.get("/list/advert/:id",userController.view_advert);
+
+router.get("/add/advert",userController.add_advert);
+
+
 module.exports = router;
